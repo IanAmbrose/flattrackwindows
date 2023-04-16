63474d1c08421abbccc67bea1274a5419b41a67b
ianambrose <ianambrose@pm.me>
2023-04-16 12:38:09 +0100

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,41 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const Group = require('../models/Group');
-const User = require('../models/User');
-const { ensureAuthenticated } = require('../config/auth');
 
-router.get('/', (req, res) => {
-  res.render('welcome');
-});
-
-router.get('/dashboard', ensureAuthenticated, async (req, res) => {
-  try {
-    const userGroups = await Group.find({ members: req.user._id });
-    const currentGroup = req.user.currentGroup;
-    res.render('dashboard', { 
-      username: req.user.username, 
-      userGroups: userGroups,
-      currentGroup: currentGroup
-    });
-  } catch (err) {
-    console.log(err);
-    req.flash('error', 'An error occurred. Please try again.');
+router.use((req, res, next) => {
+  if (!req.isAuthenticated()) {
     res.redirect('/login');
+  } else {
+    next();
   }
 });
 
-router.post('/update-current-group/:id', ensureAuthenticated, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const group = await Group.findById(req.params.id);
-    if (!group.members.includes(req.user._id)) {
-      req.flash('error', 'You cannot set this group as your current group because you are not a member');
-      res.redirect('/dashboard');
-      return;
+    const userGroups = await Group.find({ members: req.user._id });
+    res.render('dashboard', { username: req.user.username, userGroups: userGroups, messages: req.flash() });
+  } catch (err) {
+    console.log(err);
+    req.flash('error', 'An error occurred. Please try again.');
+    res.redirect('/dashboard');
+  }
+});
+
+router.post('/leave-group/:id', async (req, res) => {
+  try {
+    const groupId = req.params.id;
+    const group = await Group.findById(groupId);
+
+    if (!group) {
+      req.flash('error', 'Invalid group ID. Please try again.');
+      return res.redirect('/dashboard');
     }
-    req.user.currentGroup = req.params.id;
-    await req.user.save();
-    req.flash('success', 'Your current group has been updated');
+
+    if (!group.members.includes(req.user._id)) {
+      req.flash('error', 'You are not a member of this group.');
+      return res.redirect('/dashboard');
+    }
+
+    if (group.admin.toString() === req.user._id.toString()) {
+      req.flash('error', 'You are the admin of this group. Please delete the group to leave.');
+      return res.redirect('/dashboard');
+    }
+
+    group.members.pull(req.user._id);
+    await group.save();
+    req.flash('success', 'You have successfully left the group.');
     res.redirect('/dashboard');
   } catch (err) {
     console.log(err);
